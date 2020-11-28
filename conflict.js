71851b4b932362d2466589f79d0e8c8500071ed6
UmutAslan-cloud <umutchinax@gmail.com>
2020-11-28 21:42:32 +0100

    Birinci satir degisti ikinci satir eklendi

--- conflict.js
+++ conflict.js
@@ -1,1 +1,3 @@
-1.  // Bu aciklama main branchde eklendi. git
+  // Bu aciklama main branchde eklendi. git
+ // Bu aciklama main branchde eklendi.  Branch1de iken degistirildi
+let conflictOrnegi = "Conflict icin branchde eklendi";
